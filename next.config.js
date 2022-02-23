b94f51cd7ba3844c446839687ced26f6c24dcdaf
Tansi Jones <tansijones@outlook.com>
2022-02-23 19:49:13 +0200

    removing env from next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://Tansi-Jones:ERYxoGZkrz55znWy@learning.t5qao.mongodb.net/nekasports",
-    DEV_URL: "http://localhost:3000",
-    PROD_URL: "",
-  },
 };
