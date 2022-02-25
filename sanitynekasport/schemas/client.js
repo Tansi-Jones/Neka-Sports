5b352d4485f28f3b807fd64410e1e69b678204e1
Tansi Jones <tansijones@outlook.com>
2022-02-25 22:51:48 +0200

    Adding client schema for sanity

---
+++ sanitynekasport/schemas/client.js
@@ -0,0 +1,41 @@
+export default {
+  name: "client",
+  title: "Client",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+      date: "startDate",
+      media: "mainImage",
+    },
+  },
+};
